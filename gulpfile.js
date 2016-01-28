8b486eb9c37c2d022723dd613c12dde51a75b5a2
Charles Yang <donchoa@gmail.com>
2016-01-28 18:23:14 -0500

    Change to materialize

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,11 @@ var gulp = require('gulp');
 gulp.task('default', ['setup-public-css', 'setup-public-js']);
 
 gulp.task('setup-public-css', function() {
-  return gulp.src('./bower_components/material-design-lite/material.css')
+  return gulp.src('./bower_components/materialize/dist/css/materialize.css')
     .pipe(gulp.dest('./public/stylesheets'));
 });
 
 gulp.task('setup-public-js', function() {
-  return gulp.src('./bower_components/material-design-lite/material.js')
+  return gulp.src('./bower_components/materialize/dist/js/materialize.js')
     .pipe(gulp.dest('./public/javascripts'));
 });
